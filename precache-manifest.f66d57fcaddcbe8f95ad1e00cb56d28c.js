99512b530955652e6f676d2a849954e6bb99a87f
Yuxin Wu <yuxin_woo@yahoo.com>
2022-03-15 22:24:44 -0400

    Updates

---
+++ precache-manifest.f66d57fcaddcbe8f95ad1e00cb56d28c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081ce01055d362ccf11f381899de871e",
+    "url": "/coup-online/index.html"
+  },
+  {
+    "revision": "032cd0bd40f7d98e7d62",
+    "url": "/coup-online/static/css/main.79a0a5b5.chunk.css"
+  },
+  {
+    "revision": "5467cbacff3df4208b2d",
+    "url": "/coup-online/static/js/2.b5fcde9a.chunk.js"
+  },
+  {
+    "revision": "8a0dec406fc66684d20482d76c4bcc15",
+    "url": "/coup-online/static/js/2.b5fcde9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032cd0bd40f7d98e7d62",
+    "url": "/coup-online/static/js/main.3de703c3.chunk.js"
+  },
+  {
+    "revision": "c74be44034423944087e",
+    "url": "/coup-online/static/js/runtime-main.2f8124c6.js"
+  },
+  {
+    "revision": "ebc97bb68903da063a9c9e221f1ad194",
+    "url": "/coup-online/static/media/CheatSheet.ebc97bb6.svg"
+  },
+  {
+    "revision": "28e8baf105557932c3e1ed9dc75a9f3b",
+    "url": "/coup-online/static/media/Chicken.28e8baf1.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/coup-online/static/media/logo.ee7cd8ed.svg"
+  }
+]);
